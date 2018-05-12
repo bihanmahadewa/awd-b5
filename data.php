ae86d0cf04cab0c5177ee6a1d10cceef8ac0194c
bihanmahadewa <bihanmahadewa@gmail.com>
2018-05-12 10:01:08 +0530

    data structure

---
+++ data.php
@@ -0,0 +1,15 @@
+<?php
+
+$bill = [
+    '800 Piusha,Bihan,Kalana,Prashan,Dushanthi,Deshan,Nirash',
+    '500 Kalana,Prashan,Dushanthi,Deshan,Nirash',
+    '200 Piusha,Bihan,Deshan,Nirash',
+    '650 Piusha,Bihan,Kalana,Prashan,Dushanthi,Deshan,Nirash,Nirash',
+    '300 Dushanthi,Deshan,Nirash',
+    '275 Piusha,Piusha,Piusha,Dushanthi,Dushanthi,Deshan,Nirash',
+    '1250 Piusha,Bihan,Kalana,Prashan,Dushanthi,Deshan,Nirash,Piusha,Bihan,Kalana,Prashan,Dushanthi,Deshan,Nirash',
+    '500 Piusha,Bihan,Nirash' 
+
+
+
+]
