778fc378812bae8394decd4816bb803228393eb3
bihanmahadewa <bihanmahadewa@gmail.com>
2018-05-24 16:48:38 +0530

    homework

--- data.php
+++ data.php
@@ -1,5 +1,5 @@
 <?php
-
+     
 $bill = [
     '800 Piusha,Bihan,Kalana,Prashan,Dushanthi,Deshan,Nirash',
     '500 Kalana,Prashan,Dushanthi,Deshan,Nirash',
@@ -8,8 +8,39 @@ $bill = [
     '300 Dushanthi,Deshan,Nirash',
     '275 Piusha,Piusha,Piusha,Dushanthi,Dushanthi,Deshan,Nirash',
     '1250 Piusha,Bihan,Kalana,Prashan,Dushanthi,Deshan,Nirash,Piusha,Bihan,Kalana,Prashan,Dushanthi,Deshan,Nirash',
-    '500 Piusha,Bihan,Nirash' 
+    '500 Piusha,Bihan,Nirash'
+];
+
+
+$payer = [];
+
+
+foreach($bill as $pay){
+
+    $ammountAndUser = explode(' ',$pay);
+
+    $participants = explode(',',$ammountAndUser[1]);
+
+    $singlePersonsAmmount = $ammountAndUser[0]/count($participants);
+
+    echo "------\r\n";
+    $splitetdAmmount = [];
+    foreach($participants as $participant){
+
+        
+        //echo $participant."\r\n";
+        if(isset($splitetdAmmount[$participant])){
+            $splitetdAmmount[$participant] = $splitetdAmmount[$participant] + $singlePersonsAmmount;
+        }else{
+            $splitetdAmmount[$participant] = $singlePersonsAmmount;
+        }
+    }
+    
+    $payer[] = $splitetdAmmount; 
+ }			
 
 
 
-]
+echo "\r\n";
+ print_r($payer);
+    
---
+++ reverse.php
@@ -0,0 +1,5 @@
+<?php
+
+echo(strrev("Hello World and Coders"));
+
+?>
